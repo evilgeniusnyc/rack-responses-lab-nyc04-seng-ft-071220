8bf8069cc3bae1ca978cf6230edb4e142595b06f
evilgeniusnyc <evilgeniusnyc@me.com>
2020-08-23 11:54:46 -0400

    completed

---
+++ app/application.rb
@@ -0,0 +1,12 @@
+class Application 
+    def call(env)
+        resp = Rack:: Response.new 
+        now = Time.now
+        if now.hour <=12
+            resp.write "Good morning!"
+        else 
+            resp.write "Good afternoon!"
+        end
+        resp.finish 
+    end 
+end 
